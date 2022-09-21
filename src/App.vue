615aff36a8bb699599e1dd18701110498c274cd0
waiyan <waiyanavionics@gmail.com>
2022-09-21 17:49:28 +0630

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,60 @@
 <template>
   <div>
-    <pre>{{ pages }}</pre>
-    <div id="status">
-
-    </div>
-    <fb:login-button
-      scope="public_profile,email, pages_show_list, pages_messaging, pages_manage_metadata, pages_read_engagement"
-      onlogin="checkLoginState();"
-    >
-    </fb:login-button>
+    <button class="button" @click="logInWithFacebook">
+      Login with Facebook
+    </button>
   </div>
 </template>
-
 <script>
-function statusChangeCallback(response) {
-  // Called with the results from FB.getLoginStatus().
-  console.log("statusChangeCallback");
-  console.log(response); // The current login status of the person.
-  if (response.status === "connected") {
-    // Logged into your webpage and Facebook.
-    testAPI();
-  } else {
-    // Not logged into your webpage or we are unable to tell.
-    document.getElementById("status").innerHTML =
-      "Please log " + "into this webpage.";
-  }
-}
-
-function checkLoginState() {
-  // Called when a person is finished with the Login Button.
-  FB.getLoginStatus(function (response) {
-    // See the onlogin handler
-    statusChangeCallback(response);
-  });
-}
-
-window.fbAsyncInit = function () {
-  FB.init({
-    appId: "932010011091655",
-    cookie: true, // Enable cookies to allow the server to access the session.
-    xfbml: true, // Parse social plugins on this webpage.
-    version: "v15.0", // Use this Graph API version for this call.
-  });
-
-  FB.getLoginStatus(function (response) {
-    // Called after the JS SDK has been initialized.
-    statusChangeCallback(response); // Returns the login status.
-  });
-};
-
-function testAPI() {
-  // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-  console.log("Welcome!  Fetching your information.... ");
-  FB.api("/me", function (response) {
-    window.authUser = response;
-    console.log("Successful login for: " + response.name);
-    document.getElementById("status").innerHTML =
-      "Thanks for logging in, " + response.name + "!";
-  });
-}
-
-import axios from "axios";
 export default {
-  data() {
-    return {
-      pages: null,
-    };
-  },
-  methods: {},
-  created() {
-    axios
-      .get(
-        "https://graph.facebook.com/v15.0/3357623767804012/accounts?fields=name,access_token,picture&access_token=EAANPqH2jYscBAEZBElDjZB8GFZCLE8BOpmiy6MoxZCxp1jZBIw4G3yzZAbrqCZBwpnTz6R2Kuw3wHZAfF7yDpSjKCHl7dtvzxzylW6fCkVPDMdWf8BInXcWZAgHC81zrGteeToRHZA7sRr5jyAGyqJRG9ZBDAwW6n1pxPbz5YJRf3Sy0J85lQn1SlgbK93ZAn7M2g0ZCMgN25Ux2g15ipQZBBXn14ZBXyV7EXT3nl8ZD"
-      )
-      .then((response) => {
-        this.pages = response.data.data;
+  name: "facebookLogin",
+  methods: {
+    async logInWithFacebook() {
+      await this.loadFacebookSDK(document, "script", "facebook-jssdk");
+      await this.initFacebook();
+      window.FB.login(function (response) {
+        if (response.authResponse) {
+          alert("You are logged in &amp; cookie set!");
+          // Now you can redirect the user or do an AJAX request to
+          // a PHP script that grabs the signed request from the cookie.
+        } else {
+          alert("User cancelled login or did not fully authorize.");
+        }
       });
+      return false;
+    },
+    async initFacebook() {
+      window.fbAsyncInit = function () {
+        window.FB.init({
+          appId: "932010011091655",
+          cookie: true, // Enable cookies to allow the server to access the session.
+          xfbml: true, // Parse social plugins on this webpage.
+          version: "v15.0", // Use this Graph API version for this call.
+        });
+      };
+    },
+    async loadFacebookSDK(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    },
   },
 };
 </script>
+<style>
+.button {
+  color: white;
+  min-width: 150px;
+  background-color: #000000a1;
+  height: 2.5rem;
+  border-radius: 2rem;
+  font-weight: 400;
+  font-size: 0.8rem;
+}
+</style>
