d8a30d7bb24019b0bd1073b38a9a1368a1b55bde
waiyan <waiyanavionics@gmail.com>
2022-09-21 17:25:11 +0630

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,65 @@
 <template>
   <div>
     <pre>{{ pages }}</pre>
+    <div id="status">
 
+    </div>
     <fb:login-button
       scope="public_profile,email, pages_show_list, pages_messaging, pages_manage_metadata, pages_read_engagement"
-      :onlogin="checkLoginState"
+      onlogin="checkLoginState"
     >
     </fb:login-button>
   </div>
 </template>
 
 <script>
+function statusChangeCallback(response) {
+  // Called with the results from FB.getLoginStatus().
+  console.log("statusChangeCallback");
+  console.log(response); // The current login status of the person.
+  if (response.status === "connected") {
+    // Logged into your webpage and Facebook.
+    testAPI();
+  } else {
+    // Not logged into your webpage or we are unable to tell.
+    document.getElementById("status").innerHTML =
+      "Please log " + "into this webpage.";
+  }
+}
+
+function checkLoginState() {
+  // Called when a person is finished with the Login Button.
+  FB.getLoginStatus(function (response) {
+    // See the onlogin handler
+    statusChangeCallback(response);
+  });
+}
+
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: "932010011091655",
+    cookie: true, // Enable cookies to allow the server to access the session.
+    xfbml: true, // Parse social plugins on this webpage.
+    version: "v15.0", // Use this Graph API version for this call.
+  });
+
+  FB.getLoginStatus(function (response) {
+    // Called after the JS SDK has been initialized.
+    statusChangeCallback(response); // Returns the login status.
+  });
+};
+
+function testAPI() {
+  // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+  console.log("Welcome!  Fetching your information.... ");
+  FB.api("/me", function (response) {
+    window.authUser = response;
+    console.log("Successful login for: " + response.name);
+    document.getElementById("status").innerHTML =
+      "Thanks for logging in, " + response.name + "!";
+  });
+}
+
 import axios from "axios";
 export default {
   data() {
@@ -18,35 +67,7 @@ export default {
       pages: null,
     };
   },
-  methods: {
-    statusChangeCallback(response) {
-      // Called with the results from FB.getLoginStatus().
-      console.log("statusChangeCallback");
-      console.log(response); // The current login status of the person.
-      if (response.status === "connected") {
-        // Logged into your webpage and Facebook.
-        this.testAPI();
-      } else {
-        alert("please login")
-      }
-    },
-
-    checkLoginState() {
-      // Called when a person is finished with the Login Button.
-      FB.getLoginStatus(function (response) {
-        // See the onlogin handler
-        this.statusChangeCallback(response);
-      });
-    },
-
-    testAPI() {
-      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-      console.log("Welcome!  Fetching your information.... ");
-      FB.api("/me", function (response) {     
-        console.log("me", response) 
-      });
-    },
-  },
+  methods: {},
   created() {
     axios
       .get(
